05ab2aa442a315594b7d624d5ba641ab4fbee350
Arik Rundquist <arik.rundquist@gmail.com>
2022-08-12 21:30:24 -0500

    refactor dark theme

--- js/css.js
+++ js/css.js
@@ -2,7 +2,7 @@
 function initDark() {
     if(window.matchMedia) {
         if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.getElementsByTagName("html")[0].classList.add("dark");
+            document.documentElement.className = "dark";
         }
     }
 }
