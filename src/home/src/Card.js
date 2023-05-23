c9d76c626457cf819fec8721dcc177b18ef151bd
Arik Rundquist <arik.rundquist@gmail.com>
2023-05-22 23:02:36 -0500

    feat: reorganize structure

--- src/home/src/Card.js
+++ src/home/src/Card.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 function Card(props) {
   return (
-    <a href={props.link} className="Card" key={props.key}>
+    <a href={props.link} className="Card" key={props.key} target="_blank" rel="noopener noreferrer">
       <img className="CardImage" src={props.image} alt={props.title} />
       <h3 className="CardTitle">{props.title}</h3>
-      <p className="CardDescription" dangerouslySetInnerHTML={{"__html": `${props.description}`}}></p>
+      <p className="CardDescription">{props.description}</p>
     </a>
   );
 }
